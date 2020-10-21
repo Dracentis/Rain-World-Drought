c828dc8314c25f745e0cb663291130ad171358a1
SlimeCubed <cubedslime@gmail.com>
2020-10-21 01:14:29 -0700

    Add secondary debug log display

--- Rain World Drought/Debugging.cs
+++ Rain World Drought/Debugging.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using RWCustom;
 
@@ -8,82 +9,59 @@ namespace Rain_World_Drought
     {
         public static void Patch()
         {
-            //On.OverseersWorldAI.ShelterFinder.Update += ShelterFinder_Update;
+            On.MainLoopProcess.RawUpdate += MainLoopProcess_RawUpdate;
+            On.RainWorld.Start += RainWorld_Start;
+            On.RainWorld.Update += RainWorld_Update;
         }
 
-        private static void ShelterFinder_Update(On.OverseersWorldAI.ShelterFinder.orig_Update orig, OverseersWorldAI.ShelterFinder self)
+        private static FLabel logs;
+        private const int logLength = 10;
+        private static Queue<string> logLines;
+
+        private static void RainWorld_Start(On.RainWorld.orig_Start orig, RainWorld self)
         {
-            if (self.done)
+            orig(self);
+            logLines = new Queue<string>();
+            logs = new FLabel("font", "");
+            logs.anchorX = 0f;
+            logs.anchorY = 1f;
+            Futile.stage.AddChild(logs);
+        }
+
+        private static void RainWorld_Update(On.RainWorld.orig_Update orig, RainWorld self)
+        {
+            orig(self);
+            logs.MoveToFront();
+            logs.x = 10.5f;
+            logs.y = Futile.screen.height - 10.5f;
+        }
+
+        public static void Log(object msg) => Log(msg.ToString());
+        public static void Log(string msg)
+        {
+            string[] lines = msg.Split('\n');
+            for(int i = 0; i < lines.Length; i++)
+                logLines.Enqueue(lines[i]);
+            while (logLines.Count > logLength)
+                logLines.Dequeue();
+            logs.text = string.Join("\n", logLines.ToArray());
+        }
+
+        private static void MainLoopProcess_RawUpdate(On.MainLoopProcess.orig_RawUpdate orig, MainLoopProcess self, float dt)
+        {
+            self.myTimeStacker += dt * self.framesPerSecond;
+            int repsLeft = 10;
+            while (self.myTimeStacker > 1f)
             {
-                return;
-            }
-            if (self.checkNext.Count < 1)
-            {
-                self.NextShelter();
-            }
-            else
-            {
-                float num = float.MaxValue;
-                int num2 = -1;
-                for (int i = 0; i < self.checkNext.Count; i++)
+                self.Update();
+                self.myTimeStacker -= 1f;
+                if(repsLeft-- <= 0)
                 {
-                    float num3 = self.ResistanceOfCell(self.checkNext[i]);
-                    if (num3 > -1f && num3 < num)
-                    {
-                        num = num3;
-                        num2 = i;
-                    }
-                }
-                if (num2 < 0)
-                {
-                    self.NextShelter();
-                    return;
-                }
-                IntVector2 testCell = self.checkNext[num2];
-                self.checkNext.RemoveAt(num2);
-                AbstractRoom abstractRoom = self.world.GetAbstractRoom(testCell.x + self.world.firstRoomIndex);
-                float num4 = self.ResistanceOfCell(testCell);
-                float[] o = null;
-                for (int j = 0; j < abstractRoom.connections.Length; j++)
-                {
-                    WorldCoordinate worldCoordinate = new WorldCoordinate();
-                    float num5 = 0;
-                    if (j == testCell.y && abstractRoom.connections[j] > -1)
-                    {
-                        worldCoordinate = new WorldCoordinate(abstractRoom.connections[j], -1, -1, self.world.GetAbstractRoom(abstractRoom.connections[j]).ExitIndex(abstractRoom.index));
-                        num5 = (float)self.world.TotalShortCutLengthBetweenTwoConnectedRooms(abstractRoom.index, abstractRoom.connections[j]);
-                    }
-                    else
-                    {
-                        try
-                        {
-                            worldCoordinate = new WorldCoordinate(abstractRoom.index, -1, -1, j);
-                            num5 = (float)abstractRoom.nodes[j].ConnectionLength(testCell.y, StaticWorld.GetCreatureTemplate(CreatureTemplate.Type.Fly));
-                            if (num5 == -1f)
-                            {
-                                num5 = 10000f;
-                            }
-                        } catch(Exception e)
-                        {
-                            Debug.LogError($"Failed at 1 in room {abstractRoom.name}! {j}/[{abstractRoom.connections.Length},{abstractRoom.nodes.Length}]");
-                            Debug.LogError(e);
-                        }
-                    }
-                    try
-                    {
-                        o = self.matrix[self.currentlyMappingShelter, worldCoordinate.room - self.world.firstRoomIndex];
-                        if (num5 > -1f && o[worldCoordinate.abstractNode] == -1f)
-                        {
-                            self.matrix[self.currentlyMappingShelter, worldCoordinate.room - self.world.firstRoomIndex][worldCoordinate.abstractNode] = num4 + num5;
-                            self.checkNext.Add(new IntVector2(worldCoordinate.room - self.world.firstRoomIndex, worldCoordinate.abstractNode));
-                        }
-                    } catch(Exception e)
-                    {
-                        Debug.LogError($"Failed at 2! [{self.currentlyMappingShelter},{worldCoordinate.room - self.world.firstRoomIndex}][{worldCoordinate.abstractNode}]/[{self.matrix.GetLength(0)},{self.matrix.GetLength(0)}][{o?.Length ?? -1}]");
-                        Debug.LogError(e);
-                    }
+                    self.myTimeStacker = self.myTimeStacker % 1f;
+                    break;
                 }
             }
+            self.GrafUpdate(self.myTimeStacker);
         }
     }
 }
